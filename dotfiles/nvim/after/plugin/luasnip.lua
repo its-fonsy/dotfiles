660021de706421394a43cb12839d63009eeaa317
its-fonsy <ciabadiale@gmail.com>
2023-02-03 21:40:51 +0100

    adding luasnip configuration

---
+++ dotfiles/nvim/after/plugin/luasnip.lua
@@ -0,0 +1,32 @@
+local ls = require "luasnip"
+-- local types = require "luasnip.util.types"
+
+ls.config.set_config {
+    history = true,
+    updateevents = "TextChanged,TextChangedI",
+    enable_autosnippets = true
+}
+
+-- <c-k> as expansion key
+vim.keymap.set({ "i", "s" }, "<c-k>", function()
+    if ls.exapan_or_jumpable() then
+        ls.expand_or_jump()
+    end
+end, { silent = true })
+
+-- <c-j> backwards jump key
+vim.keymap.set({ "i", "s" }, "<c-j>", function()
+    if ls.jumpable(-1) then
+        ls.jump(-1)
+    end
+end, { silent = true })
+
+-- <c-l> to select between snippet options
+vim.keymap.set({ "i", "s" }, "<c-l>", function()
+    if ls.choice_active() then
+        ls.change_choice(1)
+    end
+end)
+
+vim.keymap.set("h", "<leader><leader>s",
+    "<cmd>source ~/.config/nvim/after/plugin/luasnip.lua<CR>")
