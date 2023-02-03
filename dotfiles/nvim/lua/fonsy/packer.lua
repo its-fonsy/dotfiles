5e1af1d26da56c7e825770652d51d5d976fb41ef
its-fonsy <ciabadiale@gmail.com>
2023-02-03 11:25:59 +0100

    nvim keybind changes

--- dotfiles/nvim/lua/fonsy/packer.lua
+++ dotfiles/nvim/lua/fonsy/packer.lua
@@ -28,12 +28,8 @@ return require('packer').startup(function(use)
         opt = true,
         config = function ()
             vim.g.vimtex_view_general_viewer = 'okular'
+            vim.g.vimtex_view_general_options = '--unique file:@pdf#src:@line@tex'
             vim.g.vimtex_view_method = 'zathura'
-            -- vim.g.vimtex_compiler_latexmk_engines = {
-            --     _ = '-xelatex'
-            -- }
-            vim.g.tex_comment_nospell = 1
-            vim.g.vimtex_compiler_progname = 'nvr'
         end,
         ft = 'tex'
     }
